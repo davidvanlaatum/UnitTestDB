d3eb28c0f6408fdf11c723703af96a1ef7a228c3
David van Laatum <david@vanlaatum.id.au>
2015-07-20 13:42:14 +0930

    Trying to make it work again

--- src/test/java/org/jenkinsci/plugins/unittestdb/TestBase.java
+++ src/test/java/org/jenkinsci/plugins/unittestdb/TestBase.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationTargetException;
 import hudson.util.Secret;
 import org.jenkinsci.plugins.database.AbstractRemoteDatabase;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author David van Laatum
  */
@@ -14,17 +16,18 @@ public class TestBase {
   public static AbstractRemoteDatabase unitTestDB () {
     if ( db == null ) {
       try {
-        db = (AbstractRemoteDatabase) Class.forName (
+          final String dbHost = System.getProperty ( "db.host" );
+          final String dbName = System.getProperty ( "db.name" );
+          final String dbUser = System.getProperty ( "db.user" );
+          final Secret dbPass = Secret.fromString ( System.getProperty ( "db.pass" ) );
+          final String dbProperties = System.getProperty ( "db.properties" );
+          requireNonNull(dbName,"Must set db.name");
+          db = (AbstractRemoteDatabase) Class.forName (
                 System.getProperty ( "db.class",
                                      "org.jenkinsci.plugins.database.mysql.MySQLDatabase" ) )
                 .getConstructor ( String.class, String.class, String.class,
                                   Secret.class, String.class )
-                .newInstance ( System.getProperty ( "db.host" ),
-                               System.getProperty ( "db.name" ),
-                               System.getProperty ( "db.user" ),
-                               Secret.fromString (
-                                       System.getProperty ( "db.pass" ) ),
-                               System.getProperty ( "db.properties" ) );
+              .newInstance ( dbHost, dbName, dbUser, dbPass, dbProperties );
       } catch ( ClassNotFoundException | NoSuchMethodException |
                 SecurityException | InstantiationException |
                 IllegalAccessException | IllegalArgumentException |
